f7cf561550c00a23b69aa3b143f883085197017e
RomanMateos <rom.suarez@duocuc.cl>
2026-05-14 22:08:12 -0400

    feat(ms-reportes): configure liquibase para que me cree la tabla de reportes sola

--- ms-reportes/reportes/src/main/java/com/fullstackev2/reportes/ReportesApplication.java
+++ ms-reportes/reportes/src/main/java/com/fullstackev2/reportes/ReportesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients; // <- IMPORTANTE
 
 @SpringBootApplication
-@EnableFeignClients // <- IMPORTANTE
+@EnableFeignClients //
 public class ReportesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReportesApplication.class, args);
