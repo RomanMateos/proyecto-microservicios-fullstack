d6611125c8d636219dc03fc12f3c9b8a047ba4be
RomanMateos <rom.suarez@duocuc.cl>
2026-05-17 18:31:45 -0400

    features (ms-sucursales arreglando errores de arranque

--- ms-sucursales/src/main/java/com/fullstackev2/SucursalesApplication.java
+++ ms-sucursales/src/main/java/com/fullstackev2/SucursalesApplication.java
@@ -1,4 +1,4 @@
-package com.fullstackev2.sucursales;
+package com.fullstackev2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
