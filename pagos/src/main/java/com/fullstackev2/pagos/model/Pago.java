e9923fc60396f47b8a4c4d1a2c04f787858bedcb
Carlos Labbe <carl.labbe@duocuc.cl>
2026-05-17 20:10:49 -0400

    Model actualizado y SLF4J

--- pagos/src/main/java/com/fullstackev2/pagos/model/Pago.java
+++ pagos/src/main/java/com/fullstackev2/pagos/model/Pago.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -13,16 +12,26 @@ import java.time.LocalDate;
 @Entity
 @Table(name="PAGOS")
 public class Pago {
+
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "metodo_pago", nullable = false, length = 50)
     private String metodoPago;
+
+    @Column(name = "detalle", nullable = false, length = 255)
     private String detalle;
+
+    @Column(name = "monto_pago", nullable = false)
     private Double montoPago;
+
+    @Column(name = "fecha_pago", nullable = false)
     private LocalDate fechaPago;
+
+    @Column(name = "aceptado", nullable = false)
     private Boolean aceptado;
 
+    @Column(name = "pedido_id", nullable = false)
     private Integer pedidoId;
-
 }
--- pagos/src/main/java/com/fullstackev2/pagos/service/PagoService.java
+++ pagos/src/main/java/com/fullstackev2/pagos/service/PagoService.java
@@ -6,13 +6,14 @@ import com.fullstackev2.pagos.dto.PedidoDTO;
 import com.fullstackev2.pagos.mapper.PagoMapper;
 import com.fullstackev2.pagos.model.Pago;
 import com.fullstackev2.pagos.repository.PagoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+@Slf4j
 @Service
 public class PagoService {
     @Autowired
