2165d1bf89db09e7e5c09259746240732f3ab5df
Carlos Labbe <carl.labbe@duocuc.cl>
2026-06-17 10:34:26 -0400

    guardar cambios

--- pagos/src/main/java/com/fullstackev2/pagos/client/PedidoClient.java
+++ pagos/src/main/java/com/fullstackev2/pagos/client/PedidoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "pedido-service",url="http://localhost:8084")
+@FeignClient(name = "pedido-service")
 public interface PedidoClient {
     @GetMapping("/api/v1/pedidos/{id}/total")
     Double obtenerTotalPedido(@PathVariable ("id") Integer id);
