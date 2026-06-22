a8009e743f7647b75f6329a278b03698738e9386
Carlos Labbe <carl.labbe@duocuc.cl>
2026-06-21 21:40:01 -0400

    properties.yml

--- usuarios/usuarios/src/main/java/com/fullstackev2/usuarios/UsuariosApplication.java
+++ usuarios/usuarios/src/main/java/com/fullstackev2/usuarios/UsuariosApplication.java
@@ -2,7 +2,9 @@ package com.fullstackev2.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UsuariosApplication {
 
