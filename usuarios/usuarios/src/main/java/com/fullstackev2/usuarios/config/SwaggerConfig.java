c895c3b97f36d52be6f8cde263e2b14622869826
Carlos Labbe <carl.labbe@duocuc.cl>
2026-06-21 21:27:48 -0400

    package config

---
+++ usuarios/usuarios/src/main/java/com/fullstackev2/usuarios/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.fullstackev2.usuarios.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Api 2026 Manejo usuario/perfil")
+                        .version("1.0")
+                        .description("Documentacion de la creacion de perfiles mediante usuarios")
+                );
+    }
+}
