c8c70f3527e54e7e5f8e25879e764a07b115a2e2
RomanMateos <rom.suarez@duocuc.cl>
2026-07-06 20:09:43 -0400

    elimine ms-pagos (duplicado obsoleto, servicio real es pagos/)

--- ms-pagos/src/main/java/com/fullstackev2/ms_pagos/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fullstackev2.ms_pagos.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
--- ms-pagos/src/main/resources/db.migration/V2__datos.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO PAGO (metodo_pago, monto, estado_pago, fecha_pago, pagado, pedido_id)
-VALUES ('Tarjeta de crédito', 150000.0, 'COMPLETADO', '2025-01-10', true, 1);
-
-INSERT INTO PAGO (metodo_pago, monto, estado_pago, fecha_pago, pagado, pedido_id)
-VALUES ('Transferencia', 89900.0, 'PENDIENTE', '2025-02-15', false, 2);
-
-INSERT INTO PAGO (metodo_pago, monto, estado_pago, fecha_pago, pagado, pedido_id)
-VALUES ('Efectivo', 45000.0, 'COMPLETADO', '2025-03-01', true, 3);
